d5b1129ffc35b2fd18e2e111a0a12e8fd296d756
Nirakar Nepal <nnepal@verra.org>
2021-10-25 13:18:48 -0500

    BookCOntroller added, getallbook, getbook, serachbook action aded

---
+++ TestBookStore/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name = {bookName} & Author = {authorName}";
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
