a2c9b38a7b1e87d2ba61acfe2e8e3f731720326e
Nirakar Nepal <nnepal@verra.org>
2021-10-27 09:08:33 -0500

    ViewData Attributes top pass data to VIew and _Layout

--- TestBookStore/Controllers/HomeController.cs
+++ TestBookStore/Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace TestBookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
+
+        [ViewData]
+        public  BookModel Book { get; set; }
         public ViewResult Index()
         {
             //ViewBag.Title = "NEP";
@@ -20,18 +25,21 @@ namespace TestBookStore.Controllers
 
             //ViewBag.Type = new BookModel() {Id=5, Author="Author" };
 
-            ViewData["property1"] = "N Nepal";
-            ViewData["book"] = new BookModel() { Author = "N Nepal", Id = 1111 };
-
+            //ViewData["property1"] = "N Nepal";
+            //ViewData["book"] = new BookModel() { Author = "N Nepal", Id = 1111 };
+            Title = "Home";
+            Book = new BookModel() { Id = 112, Title = "This Is VIew Data" };
             return View();
         }
         public ViewResult AboutUs()
         {
+            Title = "AboutUs";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            Title = "ContactUs";
             return View();
         }
     }
