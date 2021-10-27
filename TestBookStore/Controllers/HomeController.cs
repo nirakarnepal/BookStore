a113b4be75ef0bf2ccb63d8fb37737cdf8c3bdf0
Nirakar Nepal <nnepal@verra.org>
2021-10-27 08:39:57 -0500

    working with ViewBag to dynamically get data

--- TestBookStore/Controllers/HomeController.cs
+++ TestBookStore/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using TestBookStore.Models;
 
 namespace TestBookStore.Controllers
 {
@@ -10,6 +12,14 @@ namespace TestBookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = "NEP";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "NN";
+            ViewBag.Data = data;
+
+            ViewBag.Type = new BookModel() {Id=5, Author="Author" };
+
             return View();
         }
         public ViewResult AboutUs()
